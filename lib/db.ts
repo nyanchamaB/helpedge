4c72a897ea5c7e1986bc60607635518bd4c88ccc
Giddy-K <gideonkipamet@gmail.com>
2025-07-24 16:21:25 +0300

    Added temporary bypass for local dev

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,7 @@ let cached = global.mongoose || { conn: null, promise: null };
 
 export async function connectToDB() {
   if (cached.conn) return cached.conn;
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; // TEMPORARY bypass
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     bufferCommands: false,
