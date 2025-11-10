ae3426642decff8714544c518b42f0d0ca04d46d
Nyanchama Onsombi <nyanchamabrendah3@gmail.com>
2025-11-10 22:47:28 +0300

    removed @mui dependencies, implemented hooks for view and create tickets

--- client/app/onboarding/navsection.tsx
+++ client/app/onboarding/navsection.tsx
@@ -124,12 +124,16 @@ export default function NavHeader() {
           >
             Login
           </Link>
+          <Link  href="/ContactTeam" >
           <button className="px-4 py-2 border border-indigo-400 text-indigo-600 rounded-lg hover:bg-indigo-50">
             Reach out to Us
           </button>
+          </Link>
+          <Link href="/pricing/DetailsForm">
           <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-indigo-700">
             Get Started
           </button>
+          </Link>
         </div>
 
         {/* Mobile Menu Button */}
@@ -168,14 +172,18 @@ export default function NavHeader() {
           >
             Login
           </Link>
+          <Link href="/ContactTeam" >
             <button className="px-4 py-2 border border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-50">
               Reach out for support
             </button>
+          </Link>
+          <Link href="/pricing/DetailsForm" >
             <button className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700">
               Get Started
             </button>
-          </nav>
-        </div>
+          </Link>
+        </nav>
+      </div>
       )}
     </header>
   );
---
+++ client/common/form/TicketSchema.ts
@@ -0,0 +1,28 @@
+import { title } from "process";
+import {z} from "zod";
+
+export const TicketSchema = z.object({
+    title: z.string().min(1, {message: 'Title is required'}),
+    subject: z.string().min(1, {message: 'Subject is required'}),
+    description: z.string().min(1, {message: 'Description is required'}),
+    category: z.string().min(1, {message: 'Category is required'}),
+    priority: z.string().min(1, {message: 'Priority is required'}),
+    assignedTo: z.string().min(1, {message: 'Assigned to is required'}),
+    department: z.string().min(1, {message: 'Department is required'}),
+    source: z.string().min(1, {message: 'Source is required'}),
+    required_error: z.string().min(1, {message: 'Required error is required'})
+})
+
+export type Ticket = z.infer<typeof TicketSchema>;
+export const defaultValues: Ticket = {
+    title: '',
+    subject: '',
+    description: '',
+    category: '',
+    priority: '',
+    assignedTo: '',
+    department: '',
+    source: '',
+    required_error: ''
+}
+
