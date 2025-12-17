e4b85cd09bbdafa3339b78cb084a5d09cd6232da
Giddy-K <gideonkipamet@gmail.com>
2025-12-18 02:11:11 +0300

    Create index.ts

---
+++ client/components/data-table/index.ts
@@ -0,0 +1,9 @@
+export { DataTable } from './DataTable';
+export type {
+  DataTableColumn,
+  DataTableFilter,
+  DataTableAction,
+  DataTableBulkAction,
+  DataTablePagination,
+  DataTableProps,
+} from './DataTable';
