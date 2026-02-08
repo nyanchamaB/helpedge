6868722a4b5784b6e66878d8249314f72f676498
Nyanchama Onsombi <nyanchamabrendah3@gmail.com>
2026-02-08 17:26:26 +0300

    Nav page update
    Replaced the pricing with services, added mock content on the page

--- client/common/index.tsx
+++ client/common/index.tsx
@@ -44,7 +44,7 @@ export const navLinks = [
       // Add more resource links here
     ],
   },
-  { label: "Pricing", href: "/pricing" },
+  { label: "Services", href: "/services" },
 ];
 
 // Features section data
