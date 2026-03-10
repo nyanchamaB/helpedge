562f37f6661720b67d80f497074844b7bba8ec59
Giddy-K <gideonkipamet@gmail.com>
2026-03-10 23:16:29 +0300

    Update page.tsx

--- client/app/team/page.tsx
+++ client/app/team/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useEffect } from "react";
 import { useNavigation } from "@/contexts/NavigationContext";
 import { useAuth } from "@/contexts/AuthContext";
