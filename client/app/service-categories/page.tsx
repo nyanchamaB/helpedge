3247da1a78fc2fe9c9583690ae76593288024fff
Giddy-K <gideonkipamet@gmail.com>
2026-03-10 23:16:38 +0300

    Update page.tsx

--- client/app/service-categories/page.tsx
+++ client/app/service-categories/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect } from 'react';
 import { useNavigation } from '@/contexts/NavigationContext';
 import RequestCategoryTable from '@/components/service-request-category/RequestCategoryTable';
