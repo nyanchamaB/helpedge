3b645c7986ec47539b099b060c36badf6f9d73f1
Giddy-K <gideonkipamet@gmail.com>
2026-03-10 23:17:09 +0300

    Update page.tsx

--- client/app/service-categories/[id]/edit/page.tsx
+++ client/app/service-categories/[id]/edit/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { useNavigation } from '@/contexts/NavigationContext';
