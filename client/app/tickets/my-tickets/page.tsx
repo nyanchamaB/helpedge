9004e66f3ad96d4fe0f02953cfc89c2bda09390f
Giddy-K <gideonkipamet@gmail.com>
2026-03-10 23:16:14 +0300

    Update page.tsx

--- client/app/tickets/my-tickets/page.tsx
+++ client/app/tickets/my-tickets/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from "react";
 import { TicketsTable } from "@/components/tickets/TicketsTable";
 import { getTicketsByCreator, Ticket } from "@/lib/api/tickets";
