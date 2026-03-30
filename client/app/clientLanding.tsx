919b99b0e73c6a3f306aa4f78d0396014515fe45
Nyanchama Onsombi <nyanchamabrendah3@gmail.com>
2026-03-30 23:37:47 +0300

    update on landing page

---
+++ client/app/clientLanding.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import NavHeader from './onboarding/navsection';
+import HomePage from './onboarding/home';
+
+export default function ClientLandingPage() {
+  return (
+    <>
+      <NavHeader />
+      <HomePage />
+    </>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,24 +1,5 @@
-import dynamic from 'next/dynamic';
-
-// Lazy load heavy landing page components
-const NavHeader = dynamic(() => import('./onboarding/navsection'), {
-  ssr: true,
-});
-
-const HomePage = dynamic(() => import('./onboarding/home'), {
-  ssr: true,
-  loading: () => (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="animate-pulse text-lg">Loading...</div>
-    </div>
-  ),
-});
+import ClientLandingPage from './clientLanding';
 
 export default function Page() {
-  return (
-    <>
-      <NavHeader />
-      <HomePage />
-    </>
-  );
+  return <ClientLandingPage />;
 }
